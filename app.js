e14059a4a6e8223c82fed6272137fc1825d635bc
joaokbmartins <martins.joao1909@gmail.com>
2021-08-17 23:44:53 -0300

    chore: reading user input

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const validator = require('validator');
-
+const chalk = require('chalk');
 const batata = require('./utils.js');
 const getNotes = require('./notes.js');
 
 console.log('isEmail: ', validator.isEmail('test@batata.com'));
 console.log('isUrl: ', validator.isURL('https://google.com'));
 
+console.log(chalk.green('Access Granted'));
+console.log(chalk.bgYellow.red.bold('Access Denied'));
+
 console.log(batata(1, 3));
 console.log(getNotes());
+
+console.log(process.argv[2]);
