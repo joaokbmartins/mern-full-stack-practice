a8dd6fee25728886786bef4b0545bcdd748127cb
joaokbmartins <martins.joao1909@gmail.com>
2021-08-19 23:27:44 -0300

    chore: app updated to use yargs commands

--- app.js
+++ app.js
@@ -2,36 +2,7 @@ const fs = require('fs');
 const yargs = require('yargs');
 const chalk = require('chalk');
 
-const command = process.argv[2];
-
-// \
-
 const note_file_name = 'notes/' + yargs.argv['file'];
-
-addNote = () => {
-	const fileExists = fs.existsSync(note_file_name);
-	if (fileExists) {
-		logDanger('Error: file already exists...');
-		return;
-	}
-	const note_title = yargs.argv['title'];
-	const note_content = yargs.argv['body'];
-	
-	fs.writeFileSync(note_file_name, note_title.toUpperCase());
-	fs.appendFileSync(note_file_name, "\n");
-	fs.appendFileSync(note_file_name, note_content);
-	logSuccess('New note successfully created!!!');
-}
-
-deleteNote = () => {
-	try {
-		logDanger('Deleting note: ' + note_file_name);
-		fs.rmSync(note_file_name);
-		logSuccess('Note successfully deleted!!!');
-	} catch(error) {
-		logDanger('Error: ' + error.message);
-	}
-}
 
 logSuccess = (message) => {
 	console.log(chalk.green.bold.inverse(' ' + message + ' '));
@@ -41,13 +12,35 @@ logDanger = (message) => {
 	console.log(chalk.red.bold.inverse(' ' + message + ' '));
 }
 
-switch(command){
-	case 'add':
-		addNote();
-		break;
-	case 'delete':
-		deleteNote();
-		break;
-	default:
-		logDanger('Invalid option. Try agin...');
+deleteFunction = () => {
+	try {
+		logDanger('Deleting note: ' + note_file_name);
+		fs.rmSync(note_file_name);
+		logSuccess('Note successfully deleted!!!');
+	} catch(error) {
+		logDanger('Error: ' + error.message);
+	}
 }
+
+addNote = () => {
+	const note_title = yargs.argv['title'];
+	const note_content = yargs.argv['body'];
+	fs.writeFileSync(note_file_name, note_title.toUpperCase());
+	fs.appendFileSync(note_file_name, "\n");
+	fs.appendFileSync(note_file_name, note_content);
+	logSuccess('New note successfully created!!!');
+}
+
+yargs
+	.command('delete', 'Delete a note.', function() { 
+		deleteFunction();
+	})
+	.command('add', 'Create a note.', function() {
+		const fileExists = fs.existsSync(note_file_name);
+		if (fileExists) {
+			logDanger('Error: file already exists...');
+			return;
+		}
+		addNote();
+	}
+).help().argv; 
